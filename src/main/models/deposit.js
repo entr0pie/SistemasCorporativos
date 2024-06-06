ad1e979c8cfd826ef276cedcfaa03e315bda96ef
tandera <tandera@notemail.com>
2024-06-06 20:39:49 -0300

    fix: sequelize cli usage

---
+++ src/main/models/deposit.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+    Model, DataTypes
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Deposit extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+
+    Deposit.init({
+        name: {
+            type: DataTypes.STRING,
+            unique: true,
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+            allowNull: false
+        }
+    }, {
+        sequelize,
+        modelName: 'Deposit',
+    });
+    return Deposit;
+};
