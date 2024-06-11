113e37796849f6ce831f34b363abc7d128dd1b8e
tandera <tandera@notemail.com>
2024-06-10 21:32:31 -0300

    feat: added parcels to purchase request and purchase

---
+++ src/main/migrations/20240611000841-create-financial-security-to-pay.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('FinancialSecurityToPays', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      invoice: {
+        type: Sequelize.STRING
+      },
+      parcels: {
+        type: Sequelize.INTEGER
+      },
+      value: {
+        type: Sequelize.DOUBLE
+      },
+      expirationDate: {
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('FinancialSecurityToPays');
+  }
+};
