e9984e6a3ae57736ea073e21477a1d9bdd8ebf01
tandera <tandera@notemail.com>
2024-07-01 21:29:45 -0300

    docs: added description

--- src/main/routes/swagger/index.js
+++ src/main/routes/swagger/index.js
@@ -6,7 +6,6 @@ const file = fs.readFileSync("./docs/corporate_systems_swagger.yml", "utf8");
 const swaggerDocument = YAML.parse(file);
 const swaggerUi = require("swagger-ui-express");
 
-
 const router = Router();
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
